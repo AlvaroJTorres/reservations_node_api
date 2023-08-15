b2106f63b700fec305d91ed7b608bf954a54c4a3
AlvaroJTorres <mangetrone@gmail.com>
2023-08-15 18:14:32 -0500

    Add dotenv config and db run function

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
+import 'dotenv/config'
 import express from "express";
 import morgan from "morgan";
 import setRoutes from "./config/routes.js";
+import run from "./config/db.js";
 
 const app = express();
 const port = 5000;
 
 app.use(express.json());
 app.use(morgan("dev"))
+run()
 setRoutes(app);
 
 app.listen(port, () => {
