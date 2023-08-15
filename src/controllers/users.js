4539b5abd54dcd2b86433bd8333b88cc05902816
AlvaroJTorres <mangetrone@gmail.com>
2023-08-15 18:15:16 -0500

    Update index controller and add create controller for user model

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,22 @@
+import { User } from "../models/users.js";
+
 let users = [];
 
-export const index = (req, res) => {
-  res.send(users)
+export const index = async (req, res) => {
+  const users = await User.find();
+  res.send(users);
 }
+
+export const create = async (req, res) => {
+  const {name, email, role} = req.body
+
+  const newUser = new User({name, email, role})
+
+  try {
+    await newUser.save()
+    res.json(newUser)
+  } catch (e) {
+    console.error(e)
+    res.json(e)
+  }
+};
