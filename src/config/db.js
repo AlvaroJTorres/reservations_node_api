b7c771aa9383574aeac374244ac46b1b9721cf99
AlvaroJTorres <mangetrone@gmail.com>
2023-08-16 16:23:05 -0500

    Update db name and connect message

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}reservations_api_development?retryWrites=true&w=majority`;
 
 export default async function connectDB() {
   try {
     const DB = await mongoose.connect(uri);
-    console.log("Conexión de forma satisfactoria", DB.connection.name);
+    console.log(`You successfully connected to ${DB.connection.name} database`);
   } catch (e) {
     console.log(e);
   }
