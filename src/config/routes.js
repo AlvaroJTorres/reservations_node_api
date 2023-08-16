06e82a84f868b00e5d913976f6b27484a8637111
AlvaroJTorres <mangetrone@gmail.com>
2023-08-16 17:30:43 -0500

    Add restaurantRouter to the API routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,7 @@
+import { restaurantsRouter } from "../routes/restaurants.js";
 import { usersRouter } from "../routes/users.js";
 
 export default function setRoutes(app) {
+  app.use("/api/v1/restaurants", restaurantsRouter)
   app.use("/api/v1/users", usersRouter)
 }
