b29e47b2577eb297619805cd33ea7b8ca6a2dc9b
AlvaroJTorres <mangetrone@gmail.com>
2023-08-16 17:30:24 -0500

    Add restaurant routes for controller actions

---
+++ src/routes/restaurants.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import * as restaurantsController from "../controllers/restaurants.js"
+
+export const restaurantsRouter = Router();
+
+restaurantsRouter.get("/", restaurantsController.index);
+
+restaurantsRouter.post("/", restaurantsController.create);
