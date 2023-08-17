a06a2630c7d4331543eb3772d98977b716bda00e
AlvaroJTorres <mangetrone@gmail.com>
2023-08-17 17:34:08 -0500

    Update restaurant controllers to reflex changes on User model

--- src/controllers/restaurants.js
+++ src/controllers/restaurants.js
@@ -1,7 +1,8 @@
 import { Restaurant } from "../models/restaurants.js";
+import { User } from "../models/user.js";
 
 export const index = async (req, res) => {
-  const restaurants = await Restaurant.find();
+  const restaurants = await Restaurant.find().populate("manager", "name -_id");
   res.send(restaurants)
 };
 
@@ -12,6 +13,7 @@ export const create = async (req, res) => {
 
   try {
     await newRestaurant.save()
+    await User.findByIdAndUpdate(manager, {restaurant: newRestaurant})
     res.send(newRestaurant)
   } catch (e) {
     console.log(e)
@@ -28,12 +30,22 @@ export const show = async (req, res) => {
 export const update = async (req, res) => {
   const {name, address, manager} = req.body
   const id = req.params.id
-
-  const restaurant = await Restaurant.findByIdAndUpdate(id, {name, address, manager}, {new: true})
+  
+  const restaurant = await Restaurant.findById(id)
 
   if(!restaurant) return res.status(404).send(`The User with id ${id} was not found`)
 
-  res.send(restaurant)
+  const oldId = restaurant.manager._id.toHexString()
+
+  try {
+    const updatedRestaurant = await Restaurant.findByIdAndUpdate(id, {name, address, manager}, {new: true})
+    const newId = updatedRestaurant.manager._id.toHexString()
+    await User.findByIdAndUpdate(manager, {restaurant: restaurant})
+    if(oldId !== newId) await User.findByIdAndUpdate(oldId, {restaurant: null})
+    res.send(updatedRestaurant)
+  } catch(e) {
+    res.send(e)
+  }
 };
 
 export const destroy = async (req, res) => {
